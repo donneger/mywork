0f507cb4d42b824921eafcecaf37ae7e78a9fc64
Gerry Donnelly <donneger33@gmail.com>
2021-03-01 08:10:44 +0000

    Coding work in progress for week 6 lab

--- Week6 Labs/Roortest2.py
+++ Week6 Labs/Roortest2.py
@@ -1,20 +1,28 @@
 # Program to calculate Square Root using Newtons Method.
-# 
+# Newtons method uses successive iterations of the formula: root = 0.5 * (X + (N / X)) where X is any guess which can be assumed to be N or 1.
+# A desired accuracy level the user can input is also included 
+# Source https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
 
+
+
+# Define the function for calculation, the number to be rooted and the accuracy level are passed in from the program. 
+#The other variables used are initialised in the arguments as 1 and 0. 
 def sqroot (number, accuracy, test = 1, root= 0):
     
-    guess = number
-    while test > accuracy:
+    guess = number #first guess is number to be rooted. 
+    while test > accuracy: # Keep calculating until desired accuracy level is reached.
         root = (guess + number/guess)/2
         test = abs(root - guess)
         if test < accuracy:
-            print("The number input was: {} The square root of the number is: {}".format(number, root))
+            return root
         guess = root 
 
+# User inputs for the number to be rooted and accuracy level required as thi is an approximation formula. 
+number = int(input("Please input the number to be rooted:"))
+t = float(input("Please enter the accuracy level desired in %, e.g. 99.999: "))
 
-number = int(input("Please  input the number to be rooted:"))
-t = float(input("Please neter the accuracy level desired in %: "))
+# Convert the accuracy for use in the functions
 accuracy = 1-(t/100)
 
-sqroot(number, accuracy)
-
+# The sqroot function is called from the print function and the "root" value is returned and printed out. 
+print ("The number input was: {}. The Square Root of the number is {}".format(number, sqroot(number, accuracy)))
